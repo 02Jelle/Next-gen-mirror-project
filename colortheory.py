ded23c67d194478d442a154da9608944b75dc3ed
MhmdAliHdr <97683482+MhmdAliHdr@users.noreply.github.com>
2024-05-25 17:15:57 +0300

    Added comments and references to the colortheory.py file.

--- colortheory.py
+++ colortheory.py
@@ -1,4 +1,14 @@
+# Python Software Foundation, (2024), Built-in Types, Python 3.12.3 documentation, https://docs.python.org/3/library/stdtypes.html#dict, retrieved on April 19, 2024
+# Python Software Foundation (2024), Compound statements, Python 3.12.3 documentation, https://docs.python.org/3/reference/compound_stmts.html#try, retrieved April 20, 2024
+# Python Software Foundation (2024), Data model, Python 3.12.3 documentation, https://docs.python.org/3/reference/datamodel.html#index-25, retrieved April 10, 2024
+# Python Software Foundation, (2024), Data Structures, Python 3.12.3 documentation, https://docs.python.org/3/tutorial/datastructures.html, retrieved on April 12, 2024
+# Python Software Foundation (2024), Errors and Exceptions, Python 3.12.3 documentation, https://docs.python.org/3/tutorial/errors.html, retrieved April 20, 2024
+# Python Software Foundation, (2024), Python 3.12.3 documentation, Python 3.12.3 documentation, https://docs.python.org/3/, retrieved on April 10, 2024
+# Python Software Foundation, (2024), The import system, Python 3.12.3 documentation, https://docs.python.org/3/reference/import.html, retrieved on April 20, 2024
 import math
+# The following method converts the RGB values of the form [R, G, B] to HSL values of the form [H, S, L]
+# Chalaris, A. (2020), Hex to RGB, 30 Seconds of Code, https://www.30secondsofcode.org/python/s/hex-to-rgb/, retrieved April 10, 2024
+# Waldman, N. (2013), Math behind colorspace conversions, RGB-HSL, Nikolai Waldman - colorist & editor, https://www.niwa.nu/2013/05/math-behind-colorspace-conversions-rgb-hsl/, retrieved on April 12, 2024
 def rgb_to_hsl(listForm): # https://www.30secondsofcode.org/python/s/hex-to-rgb/ # https://www.niwa.nu/2013/05/math-behind-colorspace-conversions-rgb-hsl/
 	red = listForm[0]/255
 	green = listForm[1]/255
@@ -22,6 +32,12 @@ def rgb_to_hsl(listForm): # https://www.30secondsofcode.org/python/s/hex-to-rgb/
 		saturation = 0
 		hue = 0
 	return [math.ceil(hue), math.ceil(saturation * 100), math.ceil(luminance * 100)]
+# The following code applies the color difference formula to determine what the closest color from the dataset is to the given color
+# 101Computing.net (2019) Colour Difference Formula, 101Computing.net, https://www.101computing.net/colour-difference-formula/, retrieved April 10, 2024
+# Frazer Scott, Gempesaw Daniel (2012), Given a color, how do I find which color it's closest to?, StackOverflow, https://stackoverflow.com/questions/8978901/given-a-color-how-do-i-find-which-color-its-closest-to?rq=3, retrieved May 15, 2024
+# Mike, V. Karlsen L., Kiessig R., Nicholas J., Johnson N., Will, Oneill D., Donnie, et al (2009), Given an RGB value what would be the best way to find the closest match in the database?, StackOverflow, https://stackoverflow.com/questions/1847092/given-an-rgb-value-what-would-be-the-best-way-to-find-the-closest-match-in-the-d, retrieved May 15, 2024
+# Riemersma Thiadmer (2019) Color metric, CompuPhase, https://www.compuphase.com/cmetric.htm, retrieved May 17, 2024
+# Wikipedia (2024) Color difference, Wikipedia, https://en.wikipedia.org/wiki/Color_difference, retrieved May 17, 2024
 def color_difference_formula(color1, color2): # https://www.101computing.net/colour-difference-formula/
 	distance = math.sqrt(((color2[0] - color1[0]))**2 + ((color2[1] - color1[1]))**2 + ((color2[2] - color1[2]))**2)
 	return distance
@@ -66,7 +82,15 @@ def closest_color(color):
     		min_color = i
     		minimum = color_difference_formula(i, color)
     return min_color
-def complementary(color1, color2): # https://rgbcolorpicker.com/complementary # https://dev.to/madsstoumann/colors-are-math-how-they-match-and-how-to-build-a-color-picker-4ei8
+# The following code relies on the references below to apply color theory rules on the given colors
+# RGB Color Picker, Complementary Colors Generator, RGB Color Picker, https://rgbcolorpicker.com/complementary, retrieved April 10, 2024 *
+# sessions college (2008), The Sessions College Color Calculator, sessions college, https://www.sessions.edu/color-calculator/, retrieved April 12, 2024
+# Stoumann Mads (2021), Colors are Math: How they match - and how to build a Color Picker, DEV, https://dev.to/madsstoumann/colors-are-math-how-they-match-and-how-to-build-a-color-picker-4ei8, retrieved April 12, 2024
+# W3Schools Color Schemes, W3Schools, https://www.w3schools.com/colors/colors_schemes.asp, retrieved April 10, 2024 *
+# W3Schools Color Theory, W3Schools, https://www.w3schools.com/colors/colors_theory.asp, retrieved April 10, 2024 *
+# W3Schools Color Wheels, W3Schools, https://www.w3schools.com/colors/colors_wheels.asp, retrieved April 10, 2024 *
+# Complementary colors are 180 degrees away from each other
+def complementary(color1, color2):
 	hue1 = convert_to_angle(color1[0])
 	hue2 = convert_to_angle(color2[0])
 	saturation1 = color1[1]
@@ -81,14 +105,24 @@ def complementary(color1, color2): # https://rgbcolorpicker.com/complementary # 
 		return True
 	else:
 		return False
-def convert_to_angle(angle): # https://www.math10.com/en/geometry/angles/measure/angles-and-measurement.html#:~:text=Given%20a%20negative%20angle%2C%20we%20add%20360,get%20its%20corresponding%20positive%20angle.
+# convert_to_angle fixes angles so that they are positive and in the range from 0 to 360 in case they are in the negative or outside that range
+# Petrov Petrov, Y. Measuring Angles, Math10.com, https://www.math10.com/en/geometry/angles/measure/angles-and-measurement.html#:~:text=Given%20a%20negative%20angle%2C%20we%20add%20360,get%20its%20corresponding%20positive%20angle., retrieved on April 12, 2024 *
+def convert_to_angle(angle):
 	if angle >= 360:
 		return (angle - 360)
 	elif angle < 0:
 		return (angle + 360)
 	else:
 		return angle
-def split_complementary(color1, color2, color3): # https://www.sessions.edu/color-calculator/ # https://dev.to/madsstoumann/colors-are-math-how-they-match-and-how-to-build-a-color-picker-4ei8
+# The following code relies on the references below to apply color theory rules on the given colors
+# RGB Color Picker, Complementary Colors Generator, RGB Color Picker, https://rgbcolorpicker.com/complementary, retrieved April 10, 2024 *
+# sessions college (2008), The Sessions College Color Calculator, sessions college, https://www.sessions.edu/color-calculator/, retrieved April 12, 2024
+# Stoumann Mads (2021), Colors are Math: How they match - and how to build a Color Picker, DEV, https://dev.to/madsstoumann/colors-are-math-how-they-match-and-how-to-build-a-color-picker-4ei8, retrieved April 12, 2024
+# W3Schools Color Schemes, W3Schools, https://www.w3schools.com/colors/colors_schemes.asp, retrieved April 10, 2024 *
+# W3Schools Color Theory, W3Schools, https://www.w3schools.com/colors/colors_theory.asp, retrieved April 10, 2024 *
+# W3Schools Color Wheels, W3Schools, https://www.w3schools.com/colors/colors_wheels.asp, retrieved April 10, 2024 *
+# Split complimentary colors are when two colors are 150 degrees and 210 degrees away from the other one
+def split_complementary(color1, color2, color3):
 	hue1 = convert_to_angle(color1[0])
 	hue2 = convert_to_angle(color2[0])
 	hue3 = convert_to_angle(color3[0])
@@ -118,7 +152,16 @@ def split_complementary(color1, color2, color3): # https://www.sessions.edu/colo
 		return True
 	else:
 		return False
-def analogous_colors(colors): # https://www.sessions.edu/color-calculator/ # https://dev.to/madsstoumann/colors-are-math-how-they-match-and-how-to-build-a-color-picker-4ei8
+# The following code relies on the references below to apply color theory rules on the given colors
+# RGB Color Picker, Complementary Colors Generator, RGB Color Picker, https://rgbcolorpicker.com/complementary, retrieved April 10, 2024 *
+# sessions college (2008), The Sessions College Color Calculator, sessions college, https://www.sessions.edu/color-calculator/, retrieved April 12, 2024
+# Stoumann Mads (2021), Colors are Math: How they match - and how to build a Color Picker, DEV, https://dev.to/madsstoumann/colors-are-math-how-they-match-and-how-to-build-a-color-picker-4ei8, retrieved April 12, 2024
+# W3Schools Color Schemes, W3Schools, https://www.w3schools.com/colors/colors_schemes.asp, retrieved April 10, 2024 *
+# W3Schools Color Theory, W3Schools, https://www.w3schools.com/colors/colors_theory.asp, retrieved April 10, 2024 *
+# W3Schools Color Wheels, W3Schools, https://www.w3schools.com/colors/colors_wheels.asp, retrieved April 10, 2024 *
+# Analogous colors are colors that are next to each other on the color wheel, here that implies that they are within a 31 range from both sides
+# from each other
+def analogous_colors(colors):
 	if len(colors) < 2:
 		print("There are less than 2 colors provided, can not check")
 		return False
@@ -176,7 +219,15 @@ def analogous_colors(colors): # https://www.sessions.edu/color-calculator/ # htt
 			return True
 		else:
 			return False
-def triadic_colors(color1, color2, color3): # https://www.sessions.edu/color-calculator/ # https://dev.to/madsstoumann/colors-are-math-how-they-match-and-how-to-build-a-color-picker-4ei8
+# The following code relies on the references below to apply color theory rules on the given colors
+# RGB Color Picker, Complementary Colors Generator, RGB Color Picker, https://rgbcolorpicker.com/complementary, retrieved April 10, 2024 *
+# sessions college (2008), The Sessions College Color Calculator, sessions college, https://www.sessions.edu/color-calculator/, retrieved April 12, 2024
+# Stoumann Mads (2021), Colors are Math: How they match - and how to build a Color Picker, DEV, https://dev.to/madsstoumann/colors-are-math-how-they-match-and-how-to-build-a-color-picker-4ei8, retrieved April 12, 2024
+# W3Schools Color Schemes, W3Schools, https://www.w3schools.com/colors/colors_schemes.asp, retrieved April 10, 2024 *
+# W3Schools Color Theory, W3Schools, https://www.w3schools.com/colors/colors_theory.asp, retrieved April 10, 2024 *
+# W3Schools Color Wheels, W3Schools, https://www.w3schools.com/colors/colors_wheels.asp, retrieved April 10, 2024 *
+# Triadic colors are when two colors' Hues are 120 and 140 degrees away from another color
+def triadic_colors(color1, color2, color3):
 	hue1 = convert_to_angle(color1[0])
 	hue2 = convert_to_angle(color2[0])
 	hue3 = convert_to_angle(color3[0])
@@ -200,7 +251,15 @@ def triadic_colors(color1, color2, color3): # https://www.sessions.edu/color-cal
 		return True
 	else:
 		return False
-def square_colors(color1, color2, color3, color4): # https://www.sessions.edu/color-calculator/ # https://dev.to/madsstoumann/colors-are-math-how-they-match-and-how-to-build-a-color-picker-4ei8
+# The following code relies on the references below to apply color theory rules on the given colors
+# RGB Color Picker, Complementary Colors Generator, RGB Color Picker, https://rgbcolorpicker.com/complementary, retrieved April 10, 2024 *
+# sessions college (2008), The Sessions College Color Calculator, sessions college, https://www.sessions.edu/color-calculator/, retrieved April 12, 2024
+# Stoumann Mads (2021), Colors are Math: How they match - and how to build a Color Picker, DEV, https://dev.to/madsstoumann/colors-are-math-how-they-match-and-how-to-build-a-color-picker-4ei8, retrieved April 12, 2024
+# W3Schools Color Schemes, W3Schools, https://www.w3schools.com/colors/colors_schemes.asp, retrieved April 10, 2024 *
+# W3Schools Color Theory, W3Schools, https://www.w3schools.com/colors/colors_theory.asp, retrieved April 10, 2024 *
+# W3Schools Color Wheels, W3Schools, https://www.w3schools.com/colors/colors_wheels.asp, retrieved April 10, 2024 *
+# Square colors are when four colors form a square on the color wheel, so their Hue values are at 90, 180, and 270 degrees away from each other
+def square_colors(color1, color2, color3, color4):
 	hues = sorted([convert_to_angle(color1[0]), convert_to_angle(color2[0]), convert_to_angle(color3[0]), convert_to_angle(color4[0])])
 	hue1 = convert_to_angle(hues[0])
 	hue2 = convert_to_angle(hues[1])
@@ -211,7 +270,15 @@ def square_colors(color1, color2, color3, color4): # https://www.sessions.edu/co
 		return True
 	else:
 		return False
-def tetradic_rectangle(color1, color2, color3, color4): # https://www.sessions.edu/color-calculator/ # https://dev.to/madsstoumann/colors-are-math-how-they-match-and-how-to-build-a-color-picker-4ei8
+# The following code relies on the references below to apply color theory rules on the given colors
+# RGB Color Picker, Complementary Colors Generator, RGB Color Picker, https://rgbcolorpicker.com/complementary, retrieved April 10, 2024 *
+# sessions college (2008), The Sessions College Color Calculator, sessions college, https://www.sessions.edu/color-calculator/, retrieved April 12, 2024
+# Stoumann Mads (2021), Colors are Math: How they match - and how to build a Color Picker, DEV, https://dev.to/madsstoumann/colors-are-math-how-they-match-and-how-to-build-a-color-picker-4ei8, retrieved April 12, 2024
+# W3Schools Color Schemes, W3Schools, https://www.w3schools.com/colors/colors_schemes.asp, retrieved April 10, 2024 *
+# W3Schools Color Theory, W3Schools, https://www.w3schools.com/colors/colors_theory.asp, retrieved April 10, 2024 *
+# W3Schools Color Wheels, W3Schools, https://www.w3schools.com/colors/colors_wheels.asp, retrieved April 10, 2024 *
+# Tetradic colors are when four colors form a rectangle on the color wheel, so when their Hue values are at 60, 180, and 240 degrees away from each other
+def tetradic_rectangle(color1, color2, color3, color4):
 	hues = sorted([convert_to_angle(color1[0]), convert_to_angle(color2[0]), convert_to_angle(color3[0]), convert_to_angle(color4[0])])
 	hue1 = convert_to_angle(hues[0])
 	hue2 = convert_to_angle(hues[1])
@@ -222,7 +289,15 @@ def tetradic_rectangle(color1, color2, color3, color4): # https://www.sessions.e
 		return True
 	else:
 		return False
-def tints(colors): # https://dev.to/madsstoumann/colors-are-math-how-they-match-and-how-to-build-a-color-picker-4ei8
+# The following code relies on the references below to apply color theory rules on the given colors
+# RGB Color Picker, Complementary Colors Generator, RGB Color Picker, https://rgbcolorpicker.com/complementary, retrieved April 10, 2024 *
+# sessions college (2008), The Sessions College Color Calculator, sessions college, https://www.sessions.edu/color-calculator/, retrieved April 12, 2024
+# Stoumann Mads (2021), Colors are Math: How they match - and how to build a Color Picker, DEV, https://dev.to/madsstoumann/colors-are-math-how-they-match-and-how-to-build-a-color-picker-4ei8, retrieved April 12, 2024
+# W3Schools Color Schemes, W3Schools, https://www.w3schools.com/colors/colors_schemes.asp, retrieved April 10, 2024 *
+# W3Schools Color Theory, W3Schools, https://www.w3schools.com/colors/colors_theory.asp, retrieved April 10, 2024 *
+# W3Schools Color Wheels, W3Schools, https://www.w3schools.com/colors/colors_wheels.asp, retrieved April 10, 2024 *
+# Tints are colors of the same Hue and Saturation, but different Luminosity
+def tints(colors):
 	for i in range(len(colors)):
 		for j in range(len(colors)):
 			if colors[i][0] != colors[j][0] and colors[i][0] != colors[j][0] + 1 and colors[i][0] != colors[j][0] - 1:
@@ -231,8 +306,23 @@ def tints(colors): # https://dev.to/madsstoumann/colors-are-math-how-they-match-
 				return False
 	print("Tints")
 	return True
+# The following code relies on the references below to apply color theory rules on the given colors
+# RGB Color Picker, Complementary Colors Generator, RGB Color Picker, https://rgbcolorpicker.com/complementary, retrieved April 10, 2024 *
+# sessions college (2008), The Sessions College Color Calculator, sessions college, https://www.sessions.edu/color-calculator/, retrieved April 12, 2024
+# Stoumann Mads (2021), Colors are Math: How they match - and how to build a Color Picker, DEV, https://dev.to/madsstoumann/colors-are-math-how-they-match-and-how-to-build-a-color-picker-4ei8, retrieved April 12, 2024
+# W3Schools Color Schemes, W3Schools, https://www.w3schools.com/colors/colors_schemes.asp, retrieved April 10, 2024 *
+# W3Schools Color Theory, W3Schools, https://www.w3schools.com/colors/colors_theory.asp, retrieved April 10, 2024 *
+# W3Schools Color Wheels, W3Schools, https://www.w3schools.com/colors/colors_wheels.asp, retrieved April 10, 2024 *
+# Generates the complementary color for a given color
 def generate_complementary(color):
 	return [255 - color[0], 255 - color[1], 255 - color[2]]
+# The following code relies on the references below to apply color theory rules on the given colors
+# RGB Color Picker, Complementary Colors Generator, RGB Color Picker, https://rgbcolorpicker.com/complementary, retrieved April 10, 2024 *
+# sessions college (2008), The Sessions College Color Calculator, sessions college, https://www.sessions.edu/color-calculator/, retrieved April 12, 2024
+# Stoumann Mads (2021), Colors are Math: How they match - and how to build a Color Picker, DEV, https://dev.to/madsstoumann/colors-are-math-how-they-match-and-how-to-build-a-color-picker-4ei8, retrieved April 12, 2024
+# W3Schools Color Schemes, W3Schools, https://www.w3schools.com/colors/colors_schemes.asp, retrieved April 10, 2024 *
+# W3Schools Color Theory, W3Schools, https://www.w3schools.com/colors/colors_theory.asp, retrieved April 10, 2024 *
+# W3Schools Color Wheels, W3Schools, https://www.w3schools.com/colors/colors_wheels.asp, retrieved April 10, 2024 *
 def suggest_colors(list_of_colors):
     red = [255, 0, 0, 'red']
     blue = [0, 0, 255, 'blue']
@@ -252,6 +342,14 @@ def suggest_colors(list_of_colors):
         recommended_color1 = closest_color(generate_complementary(list_of_colors[0]))
         recommended_color2 = closest_color(generate_complementary(list_of_colors[1]))
         return "Try " + str(recommended_color1[3]) + " instead of " + str(closest_color(list_of_colors[1])[3]) + " or " + str(recommended_color2[3]) + " instead of " + str(closest_color(list_of_colors[0])[3])
+# The following code relies on the references below to apply color theory rules on the given colors
+# RGB Color Picker, Complementary Colors Generator, RGB Color Picker, https://rgbcolorpicker.com/complementary, retrieved April 10, 2024 *
+# sessions college (2008), The Sessions College Color Calculator, sessions college, https://www.sessions.edu/color-calculator/, retrieved April 12, 2024
+# Stoumann Mads (2021), Colors are Math: How they match - and how to build a Color Picker, DEV, https://dev.to/madsstoumann/colors-are-math-how-they-match-and-how-to-build-a-color-picker-4ei8, retrieved April 12, 2024
+# W3Schools Color Schemes, W3Schools, https://www.w3schools.com/colors/colors_schemes.asp, retrieved April 10, 2024 *
+# W3Schools Color Theory, W3Schools, https://www.w3schools.com/colors/colors_theory.asp, retrieved April 10, 2024 *
+# W3Schools Color Wheels, W3Schools, https://www.w3schools.com/colors/colors_wheels.asp, retrieved April 10, 2024 *
+# The following code combines all the previous methods to determine whether or not the colors in the list given look well together
 def color_theory(listOfColors):
 	listOfHSLColors = []
 	for i in listOfColors:
